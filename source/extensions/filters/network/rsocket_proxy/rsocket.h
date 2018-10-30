99831f4d1be0139f4ed4d1c97c3b6fd4f184bea5
linux_china <libing.chen@gmail.com>
2018-10-30 09:35:56 -0700

    rsocket filter header

---
+++ source/extensions/filters/network/rsocket_proxy/rsocket.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "envoy/network/filter.h"
+
+#include "common/common/logger.h"
+
+namespace Envoy {
+    namespace Extensions {
+        namespace NetworkFilters {
+            namespace RSocket {
+
+                /**
+                 * Implementation of a rsocket filter
+                 */
+                class RSocketFilter : public Network::Filter, Logger::Loggable<Logger::Id::filter> {
+                public:
+                    // Network::ReadFilter
+                    Network::FilterStatus onData(Buffer::Instance &data, bool end_stream) override;
+
+                    Network::FilterStatus onWrite(Buffer::Instance &data, bool end_stream) override;
+
+                    Network::FilterStatus onNewConnection() override { return Network::FilterStatus::Continue; }
+
+                    void initializeReadFilterCallbacks(Network::ReadFilterCallbacks &callbacks) override {
+                        read_callbacks_ = &callbacks;
+                    }
+
+                private:
+                    Network::ReadFilterCallbacks *read_callbacks_{};
+                };
+            } // namespace Tap
+        } // namespace NetworkFilters
+    } // namespace Extensions
+} // namespace Envoy
