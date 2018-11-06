b72531b1b69152f1eb22d38db4fa2872ac5ec026
linux_china <libing.chen@gmail.com>
2018-11-06 15:36:56 -0800

    code adjust

--- source/extensions/filters/network/rsocket_proxy/rsocket.cc
+++ source/extensions/filters/network/rsocket_proxy/rsocket.cc
@@ -19,7 +19,33 @@ namespace Envoy {
                 //on data received
                 Network::FilterStatus RSocketFilter::onData(Buffer::Instance &data, bool) {
                     ENVOY_CONN_LOG(trace, "tap: got {} bytes", read_callbacks_->connection(), data.length());
-                    //data: header + body
+                    if (isRSocketData(data)) {
+                        Frame frame{data, this->metadata_type};
+                        std::cout << "StreamId:" << frame.getStreamId() << std::endl;
+                        std::cout << "FrameLength:" << frame.getFrameLength() << std::endl;
+                        std::cout << "MetadataPresent:" << frame.isMetadataPresent() << std::endl;
+                        if (frame.getFrameType() == static_cast<byte>(0x04)) {
+                            this->stats_.frame_0x04_request_qps_.recordValue(1);
+                        }
+                        if (frame.getDataLength() > 0) {
+                            std::cout << "Data Length: " << frame.getDataLength() << std::endl;
+                            std::cout << "Data: " << frame.getDataUtf8() << std::endl;
+                        }
+                        this->stats_.request_counter_.inc();
+                    }
+                    // inject cluster name for next filter
+                    return Network::FilterStatus::Continue;
+                }
+
+                // on data write back
+                Network::FilterStatus RSocketFilter::onWrite(Buffer::Instance &data, bool) {
+                    if (isRSocketData(data)) {
+                        std::cout << "RSocket write back: " << data.length() << std::endl;
+                    }
+                    return Network::FilterStatus::Continue;
+                }
+
+                bool RSocketFilter::isRSocketData(Buffer::Instance &data) {
                     int data_len = data.length();
                     //  3 bits frame length + 4 bits stream id + 2 bits(frame type & flags)
                     if (data_len >= 9) {
@@ -30,29 +56,9 @@ namespace Envoy {
                                         | (static_cast<int>( frame_len_array[1]) << 8)
                                         | static_cast<int>( frame_len_array[2]);
                         // legal rsocket data
-                        if (frame_len + 3 == data_len) {
-                            Frame frame{data, this->metadata_type};
-                            std::cout << "StreamId:" << frame.getStreamId() << std::endl;
-                            std::cout << "FrameLength:" << frame.getFrameLength() << std::endl;
-                            std::cout << "MetadataPresent:" << frame.isMetadataPresent() << std::endl;
-                            if (frame.getFrameType() == static_cast<byte>(0x04)) {
-                                this->stats_.frame_0x04_request_qps_.recordValue(1);
-                            }
-                            if (frame.getDataLength() > 0) {
-                                std::cout << "Data Length: " << frame.getDataLength() << std::endl;
-                                std::cout << "Data: " << frame.getDataUtf8() << std::endl;
-                            }
-                            this->stats_.request_counter_.inc();
-                        }
+                        return frame_len + 3 == data_len;
                     }
-                    // inject cluster name for next filter
-                    return Network::FilterStatus::Continue;
-                }
-
-                // on data write back
-                Network::FilterStatus RSocketFilter::onWrite(Buffer::Instance &data, bool) {
-                    std::cout << "Write bytes to requester: " << data.length() << std::endl;
-                    return Network::FilterStatus::Continue;
+                    return false;
                 }
             } // namespace RSocket
         } // namespace NetworkFilters
