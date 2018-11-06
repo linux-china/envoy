146be821d86b17dc9e54fe13a8f1669fa84c34e9
linux_china <libing.chen@gmail.com>
2018-11-05 21:07:02 -0800

    adjust structure

--- source/extensions/filters/network/rsocket_proxy/config.cc
+++ source/extensions/filters/network/rsocket_proxy/config.cc
@@ -1,51 +1,34 @@
 #include <string>
 
 #include "extensions/filters/network/rsocket_proxy/rsocket.h"
+#include "extensions/filters/network/rsocket_proxy/config.h"
 #include "extensions/filters/network/well_known_names.h"
 
+#include "envoy/config/filter/network/rsocket_proxy/v2alpha1/rsocket_proxy.pb.h"
+#include "envoy/config/filter/network/rsocket_proxy/v2alpha1/rsocket_proxy.pb.validate.h"
 
 #include "envoy/registry/registry.h"
 #include "envoy/server/filter_config.h"
+#include "extensions/filters/network/common/factory_base.h"
+
 
 namespace Envoy {
     namespace Extensions {
         namespace NetworkFilters {
             namespace RSocket {
-                /**
-                 * Config registration for the tap filter. @see NamedNetworkFilterConfigFactory
-                 */
-                class RSocketConfigFactory : public Server::Configuration::NamedNetworkFilterConfigFactory {
-                public:
-                    // NamedNetworkFilterConfigFactory
-                    Network::FilterFactoryCb createFilterFactory(const Json::Object &,
-                                                                 Server::Configuration::FactoryContext &factoryContext) override {
-                        //todo factory context to add config for filter
-                        Stats::Scope &scope_ = factoryContext.scope();
-                        const std::string &prefix = this->stat_prefix;
-                        return [&](Network::FilterManager &filter_manager) -> void {
-                            filter_manager.addFilter(std::make_shared<RSocketFilter>(prefix, scope_));
-                        };
-                    }
 
-                    Network::FilterFactoryCb
-                    createFilterFactoryFromProto(const Protobuf::Message &,
-                                                 Server::Configuration::FactoryContext &factoryContext) override {
-                        //todo factory context to add config for filter
-                        Stats::Scope &scope_ = factoryContext.scope();
-                        const std::string &prefix = this->stat_prefix;
-                        return [&](Network::FilterManager &filter_manager) -> void {
-                            filter_manager.addFilter(std::make_shared<RSocketFilter>(prefix, scope_));
-                        };
-                    }
+                Network::FilterFactoryCb RSocketConfigFactory::createFilterFactoryFromProtoTyped(
+                        const envoy::extensions::filters::network::rsocket_proxy::v2alpha1::RSocketProxy &proto_config,
+                        Server::Configuration::FactoryContext &context) {
 
-                    ProtobufTypes::MessagePtr createEmptyConfigProto() override {
-                        return std::make_unique<ProtobufWkt::Empty>();
-                    }
-
-                    std::string name() override { return NetworkFilterNames::get().RSocket; }
-
-                    const std::string stat_prefix = "rsocket.";
-                };
+                    const std::string stat_prefix = fmt::format("rsocket.{}.", proto_config.stat_prefix());
+                    //todo factory context to add config for filter
+                    Stats::Scope &scope_ = context.scope();
+                    const std::string &prefix = proto_config.stat_prefix();
+                    return [&](Network::FilterManager &filter_manager) -> void {
+                        filter_manager.addFilter(std::make_shared<RSocketFilter>(prefix, scope_));
+                    };
+                }
 
                 /**
                  * Static registration for the rsocket filter. @see RegisterFactory
