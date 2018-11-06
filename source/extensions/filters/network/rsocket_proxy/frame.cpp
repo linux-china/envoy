beb5db65f32c983aebe181bce4a6515100fba0cf
linux_china <libing.chen@gmail.com>
2018-11-05 16:53:39 -0800

    add metadata & data offset

--- source/extensions/filters/network/rsocket_proxy/frame.cpp
+++ source/extensions/filters/network/rsocket_proxy/frame.cpp
@@ -6,34 +6,43 @@ namespace Envoy {
         namespace NetworkFilters {
             namespace RSocket {
                 Frame::Frame(Buffer::Instance &data) {
+                    int raw_data_len = data.length();
                     // frame length, first 3 bytes
-                    byte frameLenArray[3];
-                    data.copyOut(0, 3, frameLenArray);
-                    this->frame_len = (static_cast<int>( frameLenArray[0]) << 16)
-                                      | (static_cast<int>( frameLenArray[1]) << 8)
-                                      | static_cast<int>( frameLenArray[2]);
+                    byte frame_len_array[3];
+                    data.copyOut(0, 3, frame_len_array);
+                    this->frame_len = (static_cast<int>( frame_len_array[0]) << 16)
+                                      | (static_cast<int>( frame_len_array[1]) << 8)
+                                      | static_cast<int>( frame_len_array[2]);
                     // stream ID: 4 bytes
-                    byte streamIdArray[4];
-                    data.copyOut(3, 4, streamIdArray);
-                    this->stream_id = (static_cast<int>( streamIdArray[0]) << 16)
-                                      | (static_cast<int>( streamIdArray[1]) << 8)
-                                      | (static_cast<int>( streamIdArray[2]));
+                    byte stream_id_array[4];
+                    data.copyOut(3, 4, stream_id_array);
+                    this->stream_id = (static_cast<int>( stream_id_array[0]) << 24)
+                                      | (static_cast<int>( stream_id_array[1]) << 16)
+                                      | (static_cast<int>( stream_id_array[2]) << 8)
+                                      | (static_cast<int>( stream_id_array[3]));
                     //frame type(6 bits) && flags(10 bits)
-                    byte frameExtraArray[2];
-                    data.copyOut(7, 2, frameExtraArray);
-                    this->frame_type = frameExtraArray[0] >> 2;
-                    this->flags = (static_cast<int>(streamIdArray[0] & static_cast<byte>(3)) << 8)
-                                  | (static_cast<int>( streamIdArray[2]));
-                    this->metadata_present = (frameExtraArray[0] & static_cast<byte>(1)) == 1;
+                    byte frame_extra_array[2];
+                    data.copyOut(7, 2, frame_extra_array);
+                    this->frame_type = frame_extra_array[0] >> 2;
+                    this->flags = (static_cast<int>(frame_extra_array[0] & static_cast<byte>(3)) << 8)
+                                  | (static_cast<int>( frame_extra_array[2]));
+                    this->metadata_present = (frame_extra_array[0] & static_cast<byte>(1)) == 1;
                     //if metadata present
-                    if (this->metadata_present) {
-                        switch (this->frame_type) {
-                            case static_cast<byte>(0x04) : //REQUEST_RESPONSE
-                                std::cout << "REQUEST_RESPONSE";
-                                break;
-                            case static_cast<byte>(0x06) : //REQUEST_STREAM
-                                std::cout << "REQUEST_STREAM";
-                                break;
+                    if (this->metadata_present && raw_data_len >= 12) {
+                        ////REQUEST_RESPONSE, REQUEST_FNF (Fire-n-Forget), REQUEST_STREAM
+                        if (this->getFrameType() == static_cast<byte>(0x04)
+                            || this->getFrameType() == static_cast<byte>(0x05)
+                            || this->getFrameType() == static_cast<byte>(0x06)) {
+                            //metadata
+                            byte metadata_len_array[3];
+                            data.copyOut(9, 3, metadata_len_array);
+                            this->metadata_offset = 12;
+                            this->metadata_len = (static_cast<int>( metadata_len_array[0]) << 16)
+                                                 | (static_cast<int>( metadata_len_array[1]) << 8)
+                                                 | (static_cast<int>( metadata_len_array[2]));
+                            std::cout << "Metadata Length: " << metadata_len << std::endl;
+                            this->data_offset = 15;
+                            this->data_len = raw_data_len - this->metadata_len;
                         }
 
                     }
