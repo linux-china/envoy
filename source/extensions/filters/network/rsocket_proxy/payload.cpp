51e3c785213ecdd2a24732ad2af08a14c820e223
linux_china <libing.chen@gmail.com>
2018-11-05 19:26:12 -0800

    payload cpp

---
+++ source/extensions/filters/network/rsocket_proxy/payload.cpp
@@ -0,0 +1,11 @@
+#include "extensions/filters/network/rsocket_proxy/payload.h"
+
+namespace Envoy {
+    namespace Extensions {
+        namespace NetworkFilters {
+            namespace RSocket {
+
+            }
+        }
+    }
+}
