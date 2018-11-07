1a186fbf2783727f2f6cbe492ba4d0245c330ac9
linux_china <libing.chen@gmail.com>
2018-11-06 17:11:29 -0800

    add debug info

--- source/extensions/filters/network/rsocket_proxy/rsocket.cc
+++ source/extensions/filters/network/rsocket_proxy/rsocket.cc
@@ -21,21 +21,13 @@ namespace Envoy {
                     ENVOY_CONN_LOG(trace, "tap: got {} bytes", read_callbacks_->connection(), data.length());
                     if (isRSocketData(data)) {
                         Frame frame{data, this->metadata_type};
-                        std::cout << "StreamId:" << frame.getStreamId() << std::endl;
-                        std::cout << "FrameLength:" << frame.getFrameLength() << std::endl;
-                        std::cout << "MetadataPresent:" << frame.isMetadataPresent() << std::endl;
+                        std::cout << "==================request start===============" << std::endl;
                         if (frame.getFrameType() == static_cast<byte>(0x04)) {
                             this->stats_.frame_0x04_request_count_.inc();
                         }
-                        if (frame.getMetadataLength() > 0) {
-                            std::cout << "Metadata Length: " << frame.getMetadataLength() << std::endl;
-                            std::cout << "Metadata: " << frame.getMetadataUtf8() << std::endl;
-                        }
-                        if (frame.getDataLength() > 0) {
-                            std::cout << "Data Length: " << frame.getDataLength() << std::endl;
-                            std::cout << "Data: " << frame.getDataUtf8() << std::endl;
-                        }
+                        std::cout << frame.toString() << std::endl;
                         this->stats_.request_counter_.inc();
+                        std::cout << "==================request end===============" << std::endl;
                     }
                     // inject cluster name for next filter
                     return Network::FilterStatus::Continue;
@@ -44,17 +36,17 @@ namespace Envoy {
                 // on data write back
                 Network::FilterStatus RSocketFilter::onWrite(Buffer::Instance &data, bool) {
                     if (isRSocketData(data)) {
-                        std::cout << "RSocket write back: " << data.length() << std::endl;
+                        std::cout << "==================response start===============" << std::endl;
                         Frame frame{data, this->metadata_type};
-                        std::cout << "Response type: " << static_cast<int>(frame.getFrameType()) << std::endl;
+                        std::cout << frame.toString() << std::endl;
                         if (frame.getFrameType() == static_cast<byte>(0x0A)) {
-                            std::cout << "Metrics: 0x0A" << std::endl;
                             this->stats_.frame_0x0A_response_count_.inc();
+                            if (frame.getDataLength() > 0) {
+                                std::string dataUtf8 = frame.getDataUtf8();
+                                std::cout << "response data:" << dataUtf8 << std::endl;
+                            }
                         }
-                        if (frame.getDataLength() > 0) {
-                            std::cout << "Response Data Length: " << frame.getDataLength() << std::endl;
-                            std::cout << "Response Data: " << frame.getDataUtf8() << std::endl;
-                        }
+                        std::cout << "==================response end===============" << std::endl;
                     }
                     return Network::FilterStatus::Continue;
                 }
